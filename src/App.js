1961c181e88ff4a8b05d73ff9ce7030f8916a56d
AmriteshRaj123 <pathakamritesh6@gamil.com>
2024-06-03 21:42:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import NavFile from './Components/NavFile';
+import Home from './Components/Home';
+import Features from './Components/Features';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     < NavFile/>
+     <Home/>
+     <Features/> 
     </div>
   );
 }
 
 export default App;
+
